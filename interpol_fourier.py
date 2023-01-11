cfad7fc842a60ed2672bc1a064d37b3cde43be03
Vincent Granville <vincentg@mltechniques.com>
2023-01-11 01:27:50 -0800

    Interpolation for time series

--- interpol_fourier.py
+++ interpol_fourier.py
@@ -1,33 +1,103 @@
 import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 
-n = 100
-eps = 1.0e-8
+# https://www.digitalocean.ie/Data/DownloadTideData
 
-def g(t): 
-    alpha = 1.4    # |a| < pi/2 
-    z = np.sin(alpha*t)/(t)
+mode = 'Data' # options: 'Data', 'Math'
+ 
+IN = open("tides_Dublin.txt","r")
+table = IN.readlines()
+IN.close()
+
+temp={}
+t = 0     
+for string in table: 
+    string = string.replace('\n', '')
+    fields = string.split('\t')
+    temp[t/16]=float(fields[0])
+    t = t + 1
+nobs = len(temp)
+
+def g(t):
+    if mode == 'Data':
+        z = temp[t]
+    else:
+        z = 1.5 + np.cos(0.319*t-0.871)-0.7*np.sin(0.654*t+2.343) 
     return(z)
 
-def interpolate(t): 
+def interpolate(t, eps): 
     sum = 0
-    flag1 = -1
-    flag2 = -1
-    for k in range(1,n):
-        if k%2 == 0:
-            z = g(k) + g(-k) 
-            term = flag1 * np.sin(t*np.pi/2) 
-            flag1 = -flag1
+    t_0 = int(t + 0.5) # closest interpolation node to t
+    pi2 = 2/np.pi  
+    flag1 = -1  
+    flag2 = -1  
+    for k in range(0, n):
+        # use nodes k1, k2 in interpolation formula
+        k1 = t_0 + k
+        k2 = t_0 - k
+        tt = t - t_0
+        if k != 0: 
+            if k %2 == 0:
+                z = g(k1) + g(k2) 
+                if abs(tt**2 - k**2) > eps:
+                    term = flag1 * tt*z*pi2 * np.sin(tt/pi2) / (tt**2 - k**2)
+                else:    
+                    # use limit as tt --> k
+                    term = z/2
+                flag1 = -flag1
+            else: 
+                z = g(k1) - g(k2) 
+                if abs(tt**2 - k**2) > eps:
+                    term = flag2 * tt*z*pi2 * np.cos(tt/pi2) / (tt**2 - k**2)
+                else: 
+                    # use limit as tt --> k
+                    term = z/2
+                flag2 = -flag2
         else: 
-            z = g(k) - g(-k) 
-            term = flag2 * np.cos(t*np.pi/2) 
-            flag2 = -flag2
-        term = term*t*z/(t**2 - k**2)
+            z = g(k1)
+            if abs(tt) > eps:
+                term = z*pi2*np.sin(tt/pi2) / tt
+            else:
+                # use limit as tt --> k (here k = 0)
+                term = z
         sum += term
-    sum = (2/np.pi)*(sum + np.sin(t*np.pi/2)*g(0 + eps)/t)
     return(sum)
 
-for t in np.arange(-3, 3+eps, 0.1):  
-    z_exact = g(t + eps)
-    z_interpol = interpolate(t + eps) 
+#--- main loop
+
+t_min  = 120    # interpolate between t_min and t_max
+t_max  = 180    # interpolate between t_min and t_max
+incr   = 1/16    # time increment between nodes
+n      = 8      # 2n+1 is number of nodes used in interpolation 
+eps    = 1.0e-12 
+
+OUT = open("interpol_tides_Dublin.txt","w")
+
+time = []
+ze = []
+zi = []
+
+fig = plt.figure(figsize=(6,3))
+mpl.rcParams['axes.linewidth'] = 0.1
+mpl.rc('xtick', labelsize=6) 
+mpl.rc('ytick', labelsize=6) 
+
+for t in np.arange(t_min, t_max, incr):  
+    time.append(t)
+    z_interpol = interpolate(t, eps) 
+    z_exact = g(t)
+    zi.append(z_interpol)
+    ze.append(z_exact)
     error = abs(z_exact - z_interpol)
-    print("%10.6f %10.6f %10.6f %10.6f" % (t,z_exact,z_interpol,error))
+    if error > 0:
+        plt.scatter(t,1+error,color='black', marker='.',s=0.05)
+    if t == int(t):
+        plt.scatter(t,z_exact,color='red', s=6)
+    OUT.write("%10.6f\t%10.6f\t%10.6f\t%10.6f\n" % (t,z_exact,z_interpol,error))
+OUT.close()
+
+plt.plot(time,ze,color='red',linewidth = 1.0, alpha=0.5) ### , s=0.1)
+plt.plot(time,zi,color='blue', linewidth = 1.0,alpha=0.5) ##, s=0.1)
+plt.savefig('tides2.png', dpi=300)
+plt.show()
