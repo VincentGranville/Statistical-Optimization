16372f5c1b6306149c26301e89858720f50513a5
Vincent Granville <vincentg@mltechniques.com>
2023-01-11 16:07:10 -0800

    Interpolation for time series

--- interpol_fourier.py
+++ interpol_fourier.py
@@ -1,30 +1,48 @@
+# interpol_fourier.py (author: MLTechniques.com)
 import numpy as np
+import mpmath
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 # https://www.digitalocean.ie/Data/DownloadTideData
 
-mode = 'Data' # options: 'Data', 'Math'
- 
-IN = open("tides_Dublin.txt","r")
-table = IN.readlines()
-IN.close()
+mode = 'Data' # options: 'Data', 'Math.Bessel', 'Math.Zeta'
 
-temp={}
-t = 0     
-for string in table: 
-    string = string.replace('\n', '')
-    fields = string.split('\t')
-    temp[t/16]=float(fields[0])
-    t = t + 1
-nobs = len(temp)
+#--- read data
+
+if mode == 'Data': 
+    IN = open("tides_Dublin.txt","r")
+    table = IN.readlines()
+    IN.close()
+
+    temp={}
+    t = 0     
+    for string in table: 
+        string = string.replace('\n', '')
+        fields = string.split('\t')
+        temp[t/16]=float(fields[0])
+        t = t + 1
+    nobs = len(temp)
+
+#--- function to interpolate
 
 def g(t):
+
+    # in all 3 cases, n = 8 works with current values of other parameters
+
     if mode == 'Data':
         z = temp[t]
-    else:
-        z = 1.5 + np.cos(0.319*t-0.871)-0.7*np.sin(0.654*t+2.343) 
+    elif mode == 'Math.Bessel':
+        t = 40*(t-t_min)/(t_max-t_min) 
+        z = mpmath.besselj(1,t) 
+        z = float(z.real) # real part of the complex-valued function
+    elif mode == 'Math.Zeta': 
+        t = 4 + 40*(t-t_min)/(t_max-t_min) 
+        z = mpmath.zeta(complex(0.5,t)) 
+        z = float(z.real) # real part of the complex-valued function
     return(z)
+
+#--- interpolation function
 
 def interpolate(t, eps): 
     sum = 0
@@ -64,11 +82,11 @@ def interpolate(t, eps):
         sum += term
     return(sum)
 
-#--- main loop
+#--- main loop and visualizations
 
 t_min  = 120    # interpolate between t_min and t_max
 t_max  = 180    # interpolate between t_min and t_max
-incr   = 1/16    # time increment between nodes
+incr   = 1/16   # time increment between nodes
 n      = 8      # 2n+1 is number of nodes used in interpolation 
 eps    = 1.0e-12 
 
@@ -79,7 +97,7 @@ ze = []
 zi = []
 
 fig = plt.figure(figsize=(6,3))
-mpl.rcParams['axes.linewidth'] = 0.1
+mpl.rcParams['axes.linewidth'] = 0.2
 mpl.rc('xtick', labelsize=6) 
 mpl.rc('ytick', labelsize=6) 
 
@@ -90,14 +108,19 @@ for t in np.arange(t_min, t_max, incr):
     zi.append(z_interpol)
     ze.append(z_exact)
     error = abs(z_exact - z_interpol)
-    if error > 0:
-        plt.scatter(t,1+error,color='black', marker='.',s=0.05)
     if t == int(t):
-        plt.scatter(t,z_exact,color='red', s=6)
+        plt.scatter(t,z_exact,color='orange', s=6)
+    print("t = %8.5f exact = %8.5f interpolated = %8.5f error = %8.5f %3d nodes" % (t,z_exact,z_interpol,error,n))
     OUT.write("%10.6f\t%10.6f\t%10.6f\t%10.6f\n" % (t,z_exact,z_interpol,error))
 OUT.close()
 
-plt.plot(time,ze,color='red',linewidth = 1.0, alpha=0.5) ### , s=0.1)
-plt.plot(time,zi,color='blue', linewidth = 1.0,alpha=0.5) ##, s=0.1)
-plt.savefig('tides2.png', dpi=300)
+plt.plot(time,ze,color='red',linewidth = 0.5, alpha=0.5) 
+plt.plot(time,zi,color='blue', linewidth = 0.5,alpha=0.5)
+base = min(ze) - (max(ze) -min(ze))/10
+for index in range(len(time)):
+    # plot error bars showing delta between exact and interpolated values
+    t = time[index]
+    error = abs(zi[index]-ze[index])
+    plt.vlines(t,base,base+error,color='black',linewidth=0.2) 
+plt.savefig('tides2.png', dpi=200)
 plt.show()
